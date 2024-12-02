934dafc8225bc8695b490bfc989c422ab457fb9d
Gabe Morris <gabemorris1042@gmail.com>
2024-12-02 19:27:33 -0400

    adding contact form

---
+++ src/app/contact/components/contactForm.tsx
@@ -0,0 +1,12 @@
+
+'use client'
+
+const ContactForm = () => {
+  return (
+    <section className="py-10">
+      contact form
+    </section>
+  )
+}
+
+export default ContactForm;
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,6 +2,7 @@
 import HeroSection from '@/app/contact/components/heroSection';
 import ContactInfo from '@/app/contact/components/contactInfo';
 import FaqsSection from '@/app/contact/components/faqs';
+import ContactForm from '@/app/contact/components/contactForm';
 
 export default function Contact() {
   return (
@@ -9,6 +10,7 @@ export default function Contact() {
         <HeroSection/>
         <ContactInfo/>
         <FaqsSection/>
+        <ContactForm/>
     </main> 
   )
 }
