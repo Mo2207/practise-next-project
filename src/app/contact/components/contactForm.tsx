a0764b4b3ed46951150a02971b1e6e8b46cae30e
Gabe Morris <gabemorris1042@gmail.com>
2024-12-03 20:03:35 -0400

    building form handlers

--- src/app/contact/components/contactForm.tsx
+++ src/app/contact/components/contactForm.tsx
@@ -1,11 +1,55 @@
 
 'use client'
 
-const ContactForm = () => {
+import { useState } from 'react';
+
+interface FormData {
+  name: string,
+  email: string,
+  message: string
+}
+
+const ContactForm: React.FC = () => {
+
+  const [formData, setFormData] = useState<FormData>({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (
+    // event is specifically for input element
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    // destructuring event object
+    const {name, value} = event.target
+    // copy existing form data & set the new form data
+    setFormData({...formData, [name]: value})
+    // console.log(name, value)
+  }
+
+  // const handleFormSubmit = ()
+
   return (
-    <section className="py-10">
-      contact form
-    </section>
+    <form className="py-10 place-content-center" action="">
+      <div className='grid grid-cols-1 gap-4 content-center mx-10'>
+        <label htmlFor="">Name:
+          <input className='bg-gray-600' name="name" value={formData.name}
+          onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="">Email:
+          <input className='bg-gray-600' name="email" value={formData.email}
+          onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="">Message:
+          <input className='bg-gray-600' name="message" value={formData.message}
+          onChange={handleChange}
+          />
+        </label>
+      </div>
+    </form>
   )
 }
 
